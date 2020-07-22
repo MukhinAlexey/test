6df00827078501d51d77d20db773c178cdce4641
MukhinAlexey <mukhinalexeym@gmail.com>
2020-07-22 20:29:48 +0300

    FRN-123: Добавлена начальная структура проекта

---
+++ src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping(path = "/user", produces = "application/json")
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("{ \"name\" : \"Alexey\" }");
+    }
+}
